9cb8c7554990713dde54621b450dabf46c48bef5
davento <50033456+davento@users.noreply.github.com>
2019-08-29 08:53:55 -0500

    Completado; copiado desde el Hackerrank

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,6 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
 
 
-int main() {
-
+int main() {    
+    float s, N, a, sumA=0, sumB=0;
+    cin>>N;
+    for(int i=0;i<N;i++){
+        cin>>a;
+        sumA+=pow(a,2);
+        sumB+=a;
+    }
+    s=pow((1/N*sumA)-(pow(1/N*sumB,2)),0.5);
+    cout<<fixed<<setprecision(2)<<s;
     return 0;
 }
