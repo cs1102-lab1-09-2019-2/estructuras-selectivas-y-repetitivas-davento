06983221d8063a9abbe550beb05bf0261bd7baf5
davento <daniela.vento@utec.edu.pe>
2019-08-30 12:41:04 -0500

    Ejercicio completado en casa.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,16 @@
-
+#include <iostream>
+using namespace std;
 
 int main() {
-
+    int n, max=0;
+    cout<<"n: \n";
+    cin>>n;
+    for (int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        if (a>max)
+            max=a;
+    }
+    cout<<"El valor máximo es "<<max;
     return 0;
 }
