23f9c90941697214c39b3b5e12f268c36b2c079a
davento <daniela.vento@utec.edu.pe>
2019-08-31 00:06:03 -0500

    Ejercicio completado

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -1,7 +1,13 @@
-
+#include <iostream>
+using namespace std;
 
 int main() {
-
+    int P;
+    do{
+    cout<<"Ingrese su número";
+    cin>>P;
+    } while (P>11111111 || P<99999999)
+    
     return 0;
 }
 
--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
+using namespace std;
 
-
-int main() {
-
-    return 0;
+int score(int p, int t){
+    int x, y;
+    x=3*p/10;
+    y=p-((p/250)*t);
+    if (x>y)
+        return x;
+    if (y>x)
+        return y;
 }
 
+int main() {
+    int a, b, c, d, p, t, pfA, pfJ;
+    cout<<"Alessia resolvió el problema con la siguiente cantidad de puntos: ";
+    cin>>a;
+    cout<<"\nMinutos que demoró en presentarlo: ";
+    cin>>c;
+    cout<<"\nJasmin resolvió el problema con la siguiente cantidad de puntos: ";
+    cin>>b;
+    cout<<"\nMinutos que demoró en presentarlo: ";
+    cin>>d;
+    pfA=score(a,c);
+    pfJ=score(b,d);
+    if (pfA>pfJ)
+        cout<<"\nAlessia";
+    if (pfJ>pfA)
+        cout<<"\nJasmin";
+    if (pfJ==pfA)
+        cout<<"\nEmpate";
+    return 0;
+}
