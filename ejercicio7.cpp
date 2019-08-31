0dc9758091dc23434b52c6850fd7e20c0425058e
davento <daniela.vento@utec.edu.pe>
2019-08-31 12:29:19 -0500

    Ejercicio completado en casa

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 int main() {
-    int P;
+    int P, soat, citv, serv;
     do{
-    cout<<"Ingrese su número";
+    cout<<"Ingrese su número: ";
     cin>>P;
-    } while (P>11111111 || P<99999999);
-    
+    } while (P<11111111 || P>99999999);
+    soat=(P%100000)/10000;
+    citv=(P%100000)%10000/1000;
+    serv=(((P%10000)%10000)%1000)%100;
+    cout<<"\nSOAT: ";
+    if (soat%2==0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<"\nCITV: ";
+    if (citv%2==0)
+        cout<<"Vigente";
+    else
+        cout<<"Vencido";
+    cout<<"\nAUTORIZADO: ";
+    if (serv%2==0)
+        cout<<"Si";
+    else
+        cout<<"No";
     return 0;
 }
-
